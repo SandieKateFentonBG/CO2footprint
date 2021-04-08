16c2ad5cced600dda99bf5dde361067d384946c6
sfenton <sfenton@bollinger-grohmann.fr>
2021-04-08 20:21:29 +0200

    v2 - co2 analysis

--- polynomial_regression_02.py
+++ polynomial_regression_02.py
@@ -5,6 +5,13 @@ from IPython.display import display
 import seaborn as sns
 import folium
 
+
+"""
+------------------------------------------------------------------------------------------------------------------------
+1. DEFINITIONS
+------------------------------------------------------------------------------------------------------------------------
+"""
+
 ### 1. load & visualize full data
 
 def load_dat(filename):
@@ -25,13 +32,13 @@ def build_dictionary(X_values, y_values, x_labels, y_labels):
         dict[y_labels[i]] = y_values[:, i]
     return dict
 
-def plot_graph(dataframe, x_label, y_label, folder, drawfig==False):
+def plot_graph(dataframe, x_label, y_label, folder=None):
     sns.scatterplot(data=dataframe, x=x_label, y=y_label, hue=y_label)
-    if drawfig :
+    if folder :
         plt.savefig(folder + '/'+ x_label + '-' + y_label +'.png')
     plt.show()
 
-def draw_table(dataframe, folder, table_name):
+def draw_table(dataframe, folder=None, table_name=None):
     fig, ax = plt.subplots()
     # hide axes
     fig.patch.set_visible(False)
@@ -39,27 +46,19 @@ def draw_table(dataframe, folder, table_name):
     ax.axis('tight')
     ax.table(cellText=dataframe.values, colLabels=dataframe.columns, loc='center')
     fig.tight_layout()
-    plt.savefig(folder + '/'+ table_name +'.png')
+    if table_name :
+        plt.savefig(folder + '/'+ table_name +'.png')
     plt.show()
-
-X = load_dat("210406_cs_pm_co2/xdata_gifa_storey_span_load.csv")
-y = load_dat("210406_cs_pm_co2/ydata_totCO2.csv")
-x_labels = ['GIFA', 'STOREY', 'SPAN', 'LOAD']
-y_labels = ['CO2eq']
-all_labels = x_labels + y_labels
-CO2_dict = build_dictionary(X, y, x_labels, y_labels)
-df = pd.DataFrame(CO2_dict, columns=all_labels)
-display(df)
-#plot_graph(df, "GIFA", "CO2eq", '210406_cs_pm_co2')
-#draw_table(df, '210406_cs_pm_co2', 'CO2_values')
-#plot_graph(df, "STOREY", "CO2eq", '210406_cs_pm_co2')
-#plot_graph(df, "SPAN", "CO2eq", '210406_cs_pm_co2')
-#plot_graph(df, "LOAD", "CO2eq", '210406_cs_pm_co2')
 
 ### 2. format & visualize training data
 
+def scale_features(X):
+    # scale features by removing mean and dividing by the standard deviation
+    x_bar = np.mean(X, axis=0)
+    x_std = np.std(X, axis=0)
+    return (X - x_bar) / x_std
 
-def create_polynomial_function(X):
+def create_polynomial_features(X):
     # create virtual features
     #  raise all variables to second and third degree
     X_virtual = []
@@ -74,8 +73,6 @@ def create_polynomial_function(X):
     print(X.shape)
     return X
 
-create_polynomial_function(X)
-
 def split_dataset(X, train_ratio):
 # split training and testing dataset
     cutoff = int(X.shape[0] * train_ratio)
@@ -86,8 +83,6 @@ def split_dataset(X, train_ratio):
     print('Train/Test: %d/%d' %(X_tr.shape[0], X_te.shape[0]))
     return X_tr, y_tr, X_te, y_te
 
-X_tr, y_tr, X_te, y_te = split_dataset(X, 0.8)
-
 ### 3. regularized linear regression
 
 def regularized_linear_regression_parameters(X_set, y_set, reg_param):
@@ -97,13 +92,9 @@ def regularized_linear_regression_parameters(X_set, y_set, reg_param):
     # fit the regularized polynomial to find optimal theta matrix
     return np.matmul(pinv, y_set)
 
-theta_opt = regularized_linear_regression_parameters(X_tr, y_tr, 0.5)
-
 def predict_footprint(X_set, theta):
     # make prediction on the testing set
     return np.matmul(X_set, theta)
-
-pred = predict_footprint(X_te, theta_opt)
 
 ### 4. evaluation
 
@@ -112,37 +103,97 @@ def MSE(prediction,reference):
     mse = 0.5 * np.mean(np.square(prediction - reference))
     return mse
 
+def cost_from_mse(prediction,reference): #check!!!!!!
+    mse = MSE(prediction,reference)
+    n = len(prediction)
+    return 0.5*mse / n
+
 def MAE(prediction, reference):
     # Calculate the mean absolute error between the prediction and reference vectors
     mae = np.mean(np.abs(prediction - reference))
     return mae
 
-mse = MSE(pred, y_te)
-mae = MAE(pred, y_te)
-print(mse)
-print(mae)
-
 ### 5. Display results
-def compare_results(y_te, y_pred, folder, figure_name):
+
+def compare_results(y_te, y_pred, folder=None, figure_name=None):
     # plot the ground truth and the predicted
     x_axis = np.linspace(1,len(y_te),len(y_te))
     plt.plot(x_axis,y_te,'b',x_axis,y_pred,'r')
     plt.legend(('Ground truth','Predicted'))
-    plt.savefig(folder + '/' + figure_name + '.png')
+    if figure_name:
+        plt.savefig(folder + '/' + figure_name + '.png')
     plt.show()
 
 
-compare_results(y_te, pred, '210406_cs_pm_co2', 'truth_vs_pred')
+"""
+------------------------------------------------------------------------------------------------------------------------
+2. APPLICATION
+------------------------------------------------------------------------------------------------------------------------
+"""
+
+### 1. load & visualize full data
+
+X = load_dat("210406_cs_pm_co2/xdata_gifa_storey_span_load.csv")
+y = load_dat("210406_cs_pm_co2/ydata_totCO2.csv")
+x_labels = ['GIFA', 'STOREY', 'SPAN', 'LOAD']
+y_labels = ['CO2eq']
+all_labels = x_labels + y_labels
+CO2_dict = build_dictionary(X, y, x_labels, y_labels)
+df = pd.DataFrame(CO2_dict, columns=all_labels)
+#display(df)
+#draw_table(df)
+#plot_graph(df, "GIFA", "CO2eq")
+#plot_graph(df, "STOREY", "CO2eq", '210406_cs_pm_co2')
+#plot_graph(df, "SPAN", "CO2eq", '210406_cs_pm_co2')
+#plot_graph(df, "LOAD", "CO2eq", '210406_cs_pm_co2')
+
+### 2. format & visualize training data
+
+print ("before", X[0])
+X = scale_features(X)
+print ("after", X[0])
+y = scale_features(y)
+create_polynomial_features(X)
+X_tr, y_tr, X_te, y_te = split_dataset(X, 0.8)
+
+### 3. regularized linear regression
+
+reg = 1
+theta_opt = regularized_linear_regression_parameters(X_tr, y_tr, reg)
+pred = predict_footprint(X_te, theta_opt)
+print("regularization param : ", reg)
+
+### 4. evaluation
+
+mse = MSE(pred, y_te)
+mae = MAE(pred, y_te)
+cost = cost_from_mse(pred,y_te)
+print("Mean square error: ", mse)
+print("Mean absolute error: ", mae)
+print("Cost: ", cost)
+
+### 5. Display results
+
+compare_results(y_te, pred)
+#compare_results(y_te, pred, '210406_cs_pm_co2', 'truth_vs_pred')
 
 """
 next : 
--compute cost
+-compute cost -- check
 -predict for 1 user - value : "set one gifa", "set one span"
 -other computation ways (gradient descent,...)
 - present results/parameters associated
 -compare results/display
 -create bg data
 -analyse graphs
+- try with different parameters
+ 
+ 
+ evaluation :
+ how do we know if these values are good/bad? 
+ is this overfiting ?
+ order : split train/test - add intercept - scale - create function... ?
+
 
 """
 
